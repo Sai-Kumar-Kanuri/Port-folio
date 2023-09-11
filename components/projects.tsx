6cc1a9875a5a1b2558b9a57204cd1c446b0e2901
Sai Kumar Kanuri <saikumar.kanuri.ksvs@gmail.com>
2023-09-11 16:41:35 +0530

    added projects

---
+++ components/projects.tsx
@@ -0,0 +1,25 @@
+import React, { useRef } from "react";
+import SectionHeading from "./section-heading";
+import Project from "./project";
+import { projectsData } from "@/lib/data"
+
+
+
+const Projects = () => {
+    return (
+        <section>
+            <SectionHeading>My Projects</SectionHeading>
+            <div>
+                {projectsData.map((project, index) => (
+                    <React.Fragment key={index}>
+                        <Project {...project} />
+                    </React.Fragment>
+                ))}
+            </div>
+        </section>
+    )
+}
+
+
+
+export default Projects;
--- lib/data.ts
+++ lib/data.ts
@@ -2,106 +2,118 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
+// import corpcommentImg from "@/public/corpcomment.png";
+// import rmtdevImg from "@/public/rmtdev.png";
 import wordanalyticsImg from "@/public/wordanalytics.png";
+// import aidforlife from "@/public/aidforlife.png"
+import aidforlife from "@/public/aidforlife.png"
+import notezipper from "@/public/notezipper.png"
+import flightbooking from "@/public/flightbooking.png"
+import sentiment from "@/public/sentiment.gif"
 
 export const links = [
-  {
-    name: "Home",
-    hash: "#home",
-  },
-  {
-    name: "About",
-    hash: "#about",
-  },
-  {
-    name: "Projects",
-    hash: "#projects",
-  },
-  {
-    name: "Skills",
-    hash: "#skills",
-  },
-  {
-    name: "Experience",
-    hash: "#experience",
-  },
-  {
-    name: "Contact",
-    hash: "#contact",
-  },
+    {
+        name: "Home",
+        hash: "#home",
+    },
+    {
+        name: "About",
+        hash: "#about",
+    },
+    {
+        name: "Projects",
+        hash: "#projects",
+    },
+    {
+        name: "Skills",
+        hash: "#skills",
+    },
+    {
+        name: "Experience",
+        hash: "#experience",
+    },
+    {
+        name: "Contact",
+        hash: "#contact",
+    },
 ] as const;
 
 export const experiencesData = [
-  {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
-    description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
-    icon: React.createElement(LuGraduationCap),
-    date: "2019",
-  },
-  {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
-    description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
-    icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
-  },
-  {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
-    description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-    icon: React.createElement(FaReact),
-    date: "2021 - present",
-  },
+    {
+        title: "Graduated bootcamp",
+        location: "Miami, FL",
+        description:
+            "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+        icon: React.createElement(LuGraduationCap),
+        date: "2019",
+    },
+    {
+        title: "Front-End Developer",
+        location: "Orlando, FL",
+        description:
+            "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+        icon: React.createElement(CgWorkAlt),
+        date: "2019 - 2021",
+    },
+    {
+        title: "Full-Stack Developer",
+        location: "Houston, TX",
+        description:
+            "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+        icon: React.createElement(FaReact),
+        date: "2021 - present",
+    },
 ] as const;
 
 export const projectsData = [
-  {
-    title: "CorpComment",
-    description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
-  },
-  {
-    title: "rmtDev",
-    description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
-  },
-  {
-    title: "Word Analytics",
-    description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
-  },
+    {
+        title: "NoteZipper",
+        description:
+            "NoteZipper is a web application that allows users to create, organize, and manage their digital notes in a seamless and efficient manner.",
+        tags: ["React", "JavaScript", "Express","Nodejs", "MongoDB", "Tailwind", "Redux"],
+        imageUrl: notezipper,
+    },
+    {
+        title: "Flight Booking Portal",
+        description:
+            "eveloped a flight management system which has both admin and user roles using MERN stack.Usage of JSON Web Tokens for authentication in all the routes.",
+        tags: ["React", "MongoDB", "Express", "NodeJS","JavaScript"],
+        imageUrl: flightbooking,
+    },
+    {
+        title: "Aid For Life",
+        description:
+            "Developed a live tracking software similar to Uber or Ola where a person can quickly book an ambulance by providing the specifics of their emergency.",
+        tags: ["Flutter", "HTML", "MongoDB", "CSS", "JavaScript"],
+        imageUrl: aidforlife,
+    },
+    {
+        title: "IMDB Rating Prediction",
+        description:
+            "An end-to-end toolkit on building a movie review sentiment classification LSTM model in Keras Deep Learning",
+        tags: ["Sentiment", "NLP", "Python", "Keras", "Numpy","Flask"],
+        imageUrl: sentiment,
+    }
 ] as const;
 
 export const skillsData = [
-  "HTML",
-  "CSS",
-  "JavaScript",
-  "TypeScript",
-  "React",
-  "Next.js",
-  "Node.js",
-  "Git",
-  "Tailwind",
-  "Prisma",
-  "MongoDB",
-  "Redux",
-  "GraphQL",
-  "Apollo",
-  "Express",
-  "PostgreSQL",
-  "Python",
-  "Django",
-  "Framer Motion",
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "TypeScript",
+    "React",
+    "Next.js",
+    "Node.js",
+    "Git",
+    "Tailwind",
+    "Prisma",
+    "MongoDB",
+    "Redux",
+    "GraphQL",
+    "Apollo",
+    "Express",
+    "PostgreSQL",
+    "Python",
+    "Django",
+    "Framer Motion",
 ] as const;
